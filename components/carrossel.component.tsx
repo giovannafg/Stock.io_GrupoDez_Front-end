213ca89266cda671fc9b9e830dc2f25599a1307f
Zayra-Moraes <zayramoraes1212@gmail.com>
2026-06-15 20:56:54 -0300

    fix:ajuste para mostrar imgagens armazenadas no back

--- components/carrossel.component.tsx
+++ components/carrossel.component.tsx
@@ -36,7 +36,8 @@ export default function Carrossel({ title, items }: Props) {
                 <img src={`/lojas/${produto.loja_logo}`} className="w-[70px] object-contain" />
               </div>
               <div className="flex justify-center items-center h-[160px]">
-                <img src={`/produtos/${produto.imagem}`} className="w-[180px]" />
+                {/* <h2>{produto.imagem}</h2> */}
+                <img src={`http://localhost:3001${produto.imagem}`} className="w-[180px]" />
               </div>
               <div className="mt-6">
                 <h3 className="text-3xl font-semibold px-5">
